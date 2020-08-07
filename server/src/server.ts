a4817a20a9ad7aa0aa91f2182651660538be7201
Heuler Felipe Ramos Manfredi <heuler.manfredi@projuris.com.br>
2020-08-07 20:48:24 -0300

    :feat(SERVER): create classes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,11 @@
 import express from "express";
+import cors from "cors";
 
-import cors from 'cors';
+import routes from "./routes";
 
 const app = express();
 
+app.use(express.json());
 app.use(cors);
-
-app.get('/users', (req, resp) => {
-  console.log("Acessou aqui")
-})
-
+app.use(routes);
 app.listen(3333);
