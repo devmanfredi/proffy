e131f07734a4161f9e08193cff07c95eb33eaa1c
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-08-08 16:30:11 -0300

    update page header

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import logo from "../../assets/images/logo/logo.svg";
 import backIcon from "../../assets/images/icons/back.svg";
-import logoImg from "../../assets/images/logo.svg";
 
-import './styles.css'
+import "./styles.css";
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -15,12 +16,15 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
     <header className="page-header">
       <div className="top-bar-container">
         <Link to="/">
-          <img src={backIcon} alt="voltar" />
+          <img src={backIcon} alt="Icon to Return" />
         </Link>
-        <img src={logoImg} alt="Proffy" />
+        <img src={logo} alt="Proffy Logo" />
       </div>
+
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.description && <p>{props.description}</p>}
+
         {props.children}
       </div>
     </header>
